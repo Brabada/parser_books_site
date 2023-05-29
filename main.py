18e237f2322a513d2d1c435f0b7704c2bfd2f501
Brabada <freemindobject@gmail.com>
2023-05-30 00:03:54 +0300

    download books with titles

--- main.py
+++ main.py
@@ -1,25 +1,65 @@
 import os
 
 import requests
+from bs4 import BeautifulSoup
+from pathvalidate import sanitize_filename, sanitize_filepath
 
 
-def fetch_book(book_id):
-    books_dir = 'books'
-    os.makedirs(books_dir, exist_ok=True)
+def check_for_redirect(response):
+    main_page_url = "https://tululu.org/"
+    if response.history and response.url == main_page_url:
+        raise requests.HTTPError
 
-    url = f'https://tululu.org/txt.php?id={book_id}'
+
+def download_txt(url, filename, folder='books/'):
+    """Функция для скачивания текстовых файлов.
+    Args:
+        url (str): Cсылка на текст, который хочется скачать.
+        filename (str): Имя файла, с которым сохранять.
+        folder (str): Папка, куда сохранять.
+    Returns:
+        str: Путь до файла, куда сохранён текст.
+    """
+    os.makedirs(folder, exist_ok=True)
+
     response = requests.get(url)
     response.raise_for_status()
-    book_name = f'{book_id}.txt'
-    with open(f'{books_dir}/{book_name}', 'w') as book:
+    try:
+        check_for_redirect(response)
+    except requests.HTTPError:
+        return
+
+    filename = sanitize_filename(filename)
+    book_name = f'{filename}.txt'
+    filepath = sanitize_filepath(os.path.join(folder, book_name))
+    with open(filepath, 'w') as book:
         book.write(response.text)
+    return filepath
+
+
+def fetch_book_title(book_id):
+
+    url = f'https://tululu.org/b{book_id}/'
+    response = requests.get(url)
+    response.raise_for_status()
+
+    soup = BeautifulSoup(response.text, 'lxml')
+    title_and_author = soup.find('h1').text
+    title = title_and_author.split('::')
+    return title[0].strip()
+
+
+def download_book(book_id):
+    title = fetch_book_title(book_id)
+    book_url = f'http://tululu.org/txt.php?id={book_id}'
+    filename = f'{book_id}. {title}'
+    download_txt(book_url, filename)
 
 
 def main():
     for book_id in range(1, 10):
-        fetch_book(book_id)
+        download_book(book_id)
 
 
 if __name__ == "__main__":
     main()
-
