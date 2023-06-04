6b75c75a32645a3b55237ebfe67cffda8ee6429f
Brabada <freemindobject@gmail.com>
2023-06-04 13:34:44 +0300

    Add args support for cli

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-from pprint import pprint
 from urllib.parse import urljoin, urlsplit
+import argparse
 
 import requests
 from bs4 import BeautifulSoup
@@ -8,12 +8,14 @@ from pathvalidate import sanitize_filename, sanitize_filepath
 
 
 def check_for_redirect(response):
+    """Checking for redirect codes 3xx. If found any raises exception"""
     main_page_url = "https://tululu.org/"
     if response.history and response.url == main_page_url:
         raise requests.HTTPError
 
 
 def download_image(url, folder='images/'):
+    """Download image by url, saves its to folder and returns path to image"""
     response = requests.get(url)
     response.raise_for_status()
     os.makedirs(folder, exist_ok=True)
@@ -28,7 +30,8 @@ def download_image(url, folder='images/'):
 
 
 def download_txt(url, filename, folder='books/'):
-    """Функция для скачивания текстовых файлов.
+    """
+    Функция для скачивания текстовых файлов.
     Args:
         url (str): Cсылка на текст, который хочется скачать.
         filename (str): Имя файла, с которым сохранять.
@@ -54,24 +57,28 @@ def download_txt(url, filename, folder='books/'):
 
 
 def get_book_title(soup):
+    """Returns book title from bs4 soup"""
     title_and_author = soup.find('h1').text
     title = title_and_author.split('::')
     return title[0].strip()
 
 
 def get_book_author(soup):
+    """Returns book_author from bs4 soup"""
     title_and_author = soup.find('h1').text
     author = title_and_author.split('::')
     return author[1].strip()
 
 
 def get_book_image_url(soup, book_page_url):
+    """Returns URL for book cover by book page URL from bs4 soup"""
     img_rel_path = soup.find('div', class_='bookimage').find('img')['src']
     img_src = urljoin(book_page_url, img_rel_path)
     return img_src
 
 
 def fetch_book_page_soup(url):
+    """Fetched bs4 soup from url"""
     response = requests.get(url)
     response.raise_for_status()
     try:
@@ -82,6 +89,7 @@ def fetch_book_page_soup(url):
 
 
 def get_book_comments(soup):
+    """Return book comments[] from bs4 soup"""
     raw_comments = soup.find_all('div', class_='texts')
     comments = []
     for raw_comment in raw_comments:
@@ -90,6 +98,7 @@ def get_book_comments(soup):
 
 
 def get_book_genres(soup):
+    """Returns book genres[] from bs4 soup"""
     raw_genres = soup.find('span', class_='d_book').find_all('a')
     genres = []
     for raw_genre in raw_genres:
@@ -98,6 +107,7 @@ def get_book_genres(soup):
 
 
 def download_book(book_id):
+    """Downloads book txt and cover by book_id from tululu.org"""
     book_url = f'https://tululu.org/b{book_id}/'
     soup = fetch_book_page_soup(book_url)
     if not soup:
@@ -110,11 +120,10 @@ def download_book(book_id):
     if image_src:
         download_image(image_src)
     download_txt(book_url, filename)
-    pprint(parse_book_page(soup))
 
 
 def parse_book_page(soup):
-    """Returns dict with author, title, genre"""
+    """Returns dict with author, title, genres[], comments[]"""
 
     book_info = {
         'author': get_book_author(soup),
@@ -125,8 +134,29 @@ def parse_book_page(soup):
     return book_info
 
 
+def init_arg_parser():
+    parser = argparse.ArgumentParser(
+        description='Script is download books from tululu.org by their id.')
+    parser.add_argument(
+        '-s',
+        '--start_id',
+        help='book_id for range start',
+        default=1,
+        type=int)
+    parser.add_argument(
+        '-e',
+        '--end_id',
+        help='book_id for range end',
+        default=1,
+        type=int)
+    return parser
+
+
 def main():
-    for book_id in range(1, 10):
+    parser = init_arg_parser()
+    args = parser.parse_args()
+
+    for book_id in range(args.start_id, args.end_id+1):
         download_book(book_id)
 
 
